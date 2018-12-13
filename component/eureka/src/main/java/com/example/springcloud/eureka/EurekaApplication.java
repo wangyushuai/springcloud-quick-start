9e242cf1ecd31988ed1a29e5ffb7245f903bcc77
wangyushuai <wangyushuai@fang.com>
2018-12-13 15:36:19 +0800

    添加Eureka注册中心

---
+++ component/eureka/src/main/java/com/example/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
+
