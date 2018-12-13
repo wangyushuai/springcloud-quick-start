91fb680ab756b95a1bc307b019ec02c362a134ce
wangyushuai <wangyushuai@fang.com>
2018-12-13 11:21:03 +0800

    修改启动类包扫描路径，添加工具类扫描

--- service/order-service/src/main/java/com/example/springcloud/orderservice/OrderServiceApplication.java
+++ service/order-service/src/main/java/com/example/springcloud/orderservice/OrderServiceApplication.java
@@ -2,10 +2,13 @@ package com.example.springcloud.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+//修改默认扫描包
+@ComponentScan(basePackages = {"com.example.springcloud.orderservice","com.example.springcloud.util"})
 public class OrderServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
