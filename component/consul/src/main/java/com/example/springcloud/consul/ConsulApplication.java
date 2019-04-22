57aceac80e6a180bf6daccb449b9d6f0872139b6
wangyushuai <wangyushuai2@jd.com>
2019-04-22 21:21:22 +0800

    组件添加 consul 模块

---
+++ component/consul/src/main/java/com/example/springcloud/consul/ConsulApplication.java
@@ -0,0 +1,11 @@
+package com.example.springcloud.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulApplication.class, args);
+    }
+}
