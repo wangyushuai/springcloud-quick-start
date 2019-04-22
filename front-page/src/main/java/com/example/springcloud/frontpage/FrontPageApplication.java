40300c72705284e6e2ce895fa2912b83956519c3
wangyushuai <wangyushuai2@jd.com>
2019-04-22 10:17:21 +0800

    1. 调整前端页面模块路径 2. 前端页面模块：添加图片上传功能

---
+++ front-page/src/main/java/com/example/springcloud/frontpage/FrontPageApplication.java
@@ -0,0 +1,35 @@
+package com.example.springcloud.frontpage;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FrontPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontPageApplication.class, args);
+    }
+
+
+    /**
+     * tomcatEmbedded 这段代码是为了解决，上传文件大于10M出现连接重置的问题。此异常内容 GlobalException 也捕获不到。
+     * @return
+     */
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
+    }
+
+
+}
